eaeeeb06816313988e9a8bdb677bab2e6302ffe5
Anateon <Anateon@hotmail.com>
2024-06-27 00:26:35 +0500

    PWA fix

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,38 @@
-const CACHE_NAME = 'calculator-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js'
+const cacheName = 'calculator-cache-v1';
+const appShellFiles = [
+  '/bank-calculator/',
+  '/bank-calculator/index.html',
+  '/bank-calculator/style.css',
+  '/bank-calculator/script.js',
+  '/bank-calculator/icon/icon-192.png',
+  '/bank-calculator/icon/icon-512.png',
+  '/bank-calculator/icon/icon.svg',
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+  });
+  
+  self.addEventListener('fetch', (e) => {
+      if (!(
+         e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+      )) {
+          return; 
+      }
+  
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) return r;
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
