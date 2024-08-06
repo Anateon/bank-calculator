8df6e0c33ae123e786d666faac2aa13397b34618
Anateon <Anateon@hotmail.com>
2024-08-06 16:38:12 +0500

    Service worker cache fix

--- service-worker.js
+++ service-worker.js
@@ -12,27 +12,48 @@ const appShellFiles = [
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil((async () => {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(appShellFiles);
-    })());
-  });
-  
-  self.addEventListener('fetch', (e) => {
-      if (!(
-         e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
-      )) {
-          return; 
+      try {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(appShellFiles);
+      } catch (err) {
+        console.error('[Service Worker] Caching failed', err);
       }
-  
+    })());
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Activate');
+    e.waitUntil((async () => {
+        const keyList = await caches.keys();
+        await Promise.all(keyList.map((key) => {
+            if (key !== cacheName) {
+                console.log('[Service Worker] Removing old cache', key);
+                return caches.delete(key);
+            }
+        }));
+        await self.clients.claim();
+    })());
+});
+
+self.addEventListener('fetch', (e) => {
+    if (!(e.request.url.startsWith('http:') || e.request.url.startsWith('https:'))) {
+        return;
+    }
+
     e.respondWith((async () => {
       const r = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) return r;
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+      try {
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        await cache.put(e.request, response.clone());
+        return response;
+      } catch (err) {
+        console.error(`[Service Worker] Fetch failed: ${e.request.url}`, err);
+        throw err;
+      }
     })());
-  });
+});
